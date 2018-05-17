c23ea1ea8e669b338d96b0420b57258273c7887c
nieyue <278076304@qq.com>
2018-05-17 18:14:11 +0800

    2018 5 17

---
+++ src/com/nieyue/gn/test_all.java
@@ -0,0 +1,286 @@
+package com.nieyue.gn;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Vector;
+//--------------------------------------------------------------
+//有连接的边，x和y为边的端点，value为权值
+class v_side{
+	int x;
+	int y;
+	double value;//这个value数组是用来存放计算过程中以某个源点出发对各边的介数
+	double value2;//value2数组是用来存放最终所有的介数之和
+	boolean remove_if;//在GN算法中决定去掉介数最大的边，这一项设置是否去掉，去掉这条边的时候对于此项设置为true
+
+	v_side(int x,int y, double value){
+		int x1 = ( (x< y) ? x:y);
+		int y1 = ( (x> y) ? x:y);
+		this.x = x1;//小的值给this.x
+		this.y = y1;//大的值给this.y
+		this.value = value;
+		this.value2 = value;
+		this.remove_if = false;//这条边是否在算法处理过程中被删除掉
+	}
+
+}
+//--------------------------------------------------------------
+/*这个类是用来存放每次分裂点隶属情况，*/
+class point_belong{
+	double Q;//记录这种分裂情况下的Q值。
+	int []point_belong_to;//记录这种分裂情况下，每个点的属于哪个集团。例如点0属于集团2，则有point_belong_to[0]=2
+	point_belong(int num, double Q){
+		this.Q = Q;
+		this.point_belong_to = new int[num];
+	}
+}
+//--------------------------------------------------------------
+/*这个类用来记录每次被删除的边*/
+class s_remove_belong{
+	int x;//边的点值x和y。
+	int y;
+	int belong_clique;//这条边是在分裂为几个集团的时候被删除掉的。比如v(i,j)是在分裂为两个集团时去除的,则x=i,y=j;belong_clique=2;
+
+	s_remove_belong(int x, int y, int belong_clique){
+		this.x = x;
+		this.y = y;
+		this.belong_clique = belong_clique;
+	}
+}
+//--------------------------------------------------------------
+public class test_all {
+	public static int vertexNumber=0;//顶点数
+	public static int[][] adjacencyList;//邻接表
+	public static List<int[]> Edge_graph;//邻接表
+	public static int[][] adjacencyMatrix;//邻接矩阵
+	 /**
+     * 邻接表转邻接矩阵 
+     * @param adjacencyList 邻接表
+     */
+    public static void adjacencyListChangeadjacencyMatrix(int[][] adjacencyList,int vertexNumber) {
+    	adjacencyMatrix=new int[vertexNumber][vertexNumber];
+    	//邻接表转邻接矩阵
+    	for(int i=0;i<adjacencyList.length;i++){
+    		adjacencyMatrix[adjacencyList[i][0]-1][adjacencyList[i][1]-1]=1;
+    		adjacencyMatrix[adjacencyList[i][1]-1][adjacencyList[i][0]-1]=1;
+		}
+    /*	System.out.println("邻接表转邻接矩阵 :");
+    	for(int i=0;i<adjacencyMatrix.length;i++)  
+    	    System.out.println(Arrays.toString(adjacencyMatrix[i]));*/
+    }
+	public static double getQ(HashMap<Integer,List<Integer>> Communitys,List<int[]> Edge_graph,int [][] Adjmartrix) {
+		double q = 0;
+		int communityNum = Communitys.size();
+		int[][] communityMtr = new int[communityNum][communityNum];
+		List<Integer> iList = null;
+		List<Integer> jList = null;
+		List<Integer> keyList = new ArrayList<>(Communitys.keySet());
+		for (int i_index=0; i_index<keyList.size();i_index++ ) {
+			iList = Communitys.get(keyList.get(i_index));
+			for (int j_index=0;j_index<keyList.size();j_index++) {
+				jList = Communitys.get(keyList.get(j_index));
+				for(int i=0;i<iList.size()-1;i++){
+					for(int j=0;j<jList.size();j++){
+						int i4=iList.get(i);
+						int j4=jList.get(j);
+						communityMtr[i_index][j_index] += Adjmartrix[i4-1][j4-1];
+					}
+				}
+			}
+		}
+		float a=0,e=0;
+		double a_2 = 0;
+		for(int i=0;i<communityNum;i++){
+			e += communityMtr[i][i];
+			for(int j=0;j<communityNum;j++){
+				if(i==j)
+					continue;
+				a += communityMtr[i][j];
+			}
+			a_2 += ((0.5*a/(float)Edge_graph.size())*(0.5*a/(float)Edge_graph.size()));
+		}
+	/*	System.out.println("e=" + e + " a=" + a);
+		System.out.println("e=" + e + " a=" + a + " a_2=" + a_2);*/
+		q = e/Edge_graph.size()*0.5 - a_2;
+		return q;
+	}
+	public static void main(String args[])throws IOException{
+		long start=System.currentTimeMillis();
+		int i,j;
+		Vector<v_side> vec_relationship  = new Vector<v_side>();//记录有链接的边。
+		int[] r_sign;//记录矩阵中行号为i的元素（边）在vec_relationship 中出现的首索引
+		int max=0;//这个是max是点的个数
+		
+		
+		String ecoding = "gbk";
+		//File file = new File("src/com/nieyue/gn/Karate.txt");
+		//File file = new File("src/com/nieyue/gn/input4.txt");
+		File file = new File("src/com/nieyue/gn/Dolphins.txt");
+		//File file = new File("src/com/nieyue/gn/Football.txt");
+		if(file.isFile() && file.exists() ){
+			InputStreamReader read = new InputStreamReader( new FileInputStream(file),ecoding);
+			BufferedReader bufferedreader = new BufferedReader(read);
+			String Text_line = null;
+			Text_line = bufferedreader.readLine();
+			String[] max_str=Text_line.split("-");
+			max=Integer.parseInt(max_str[0]);
+			//存放边
+			List<List<Integer>> ll=new ArrayList<>();
+			while( ( Text_line = bufferedreader.readLine() ) != null ){
+				String text = Text_line.replace(",", "");
+				String[] version=text.split("-");
+				
+				for(int k = 0; k < version.length; k++ ){
+					String[] version2 = version[k].split(":");
+					int x1=Integer.parseInt(version2[0]);
+					int x2=Integer.parseInt(version2[1]);
+					v_side v_sd = new v_side(x1-1,x2-1,1);
+					vec_relationship.add(v_sd);
+					List<Integer> l=new ArrayList<>();
+					l.add(x1);
+					l.add(x2);
+					ll.add(l);
+				}
+			}
+			//放置adjacencyList
+			adjacencyList=new int[ll.size()][];
+			Edge_graph=new ArrayList<>();
+			Set<Integer> set=new HashSet<Integer>();
+			for (int k = 0; k < ll.size(); k++) {
+				int[] a=new int[ll.get(k).size()];
+				for (int k2 = 0; k2 < ll.get(k).size(); k2++) {
+					a[k2]=ll.get(k).get(k2);
+					set.add(ll.get(k).get(k2));
+				}
+				adjacencyList[k]=a;
+				Edge_graph.add(a);
+			}
+			//顶点个数
+			vertexNumber=set.size();
+			read.close();
+			bufferedreader.close();
+		}
+		
+		r_sign = new int[max];//给r_sign设置数组大小
+		//r_sign数组先统一赋值为-1
+		for(i = 0; i < max; i++){
+			r_sign[i] = -1;
+		} 
+		GN_use.v_order( vec_relationship, r_sign);//对vec_relationship中的内容进行排序整理,
+
+		/*下面是GN算法的实现*/
+
+		//当Q值最大时，Q_i存放数组result_temp[][]中对应的行号i，以便在输出结果的时候容易找到。		
+		int Q_i=0;
+		//result_temp_i是在存储各种Q值下分裂得到各个点的社团隶属情况要用到的行号下标变量，也就是数组result_temp[][]的行号下标，最后一次的分裂情况下的行号记录在result_temp_i中。
+		int result_temp_i=0;
+		//vec_result_temp数组每行下标0到max-1的是存放每个点隶属的集团号，存放每种Q 值下的分裂情况，每行对应一种情况，元素vec_result_temp.Q存放对应的Q值，		
+		Vector< point_belong> vec_result_temp = new Vector< point_belong>(); 
+		//vec_V_remove_belong数组用来存放每次去除介数最大的边的隶属情况，比如v(i,j)是在分裂为两个集团时去除的，则vec_V_remove_belong.x=i-1,vec_V_remove_belong.x=j-1，若是分裂为三个集团时去除的，则vec_V_remove_belong.belong_clique值为3
+		Vector< s_remove_belong > vec_V_remove_belong = new Vector< s_remove_belong >();
+		//若原始网络本身就是由几个孤立的社团构成的，original_community_alones+2是记录原始网络的孤立社团数目，
+		//在vec_result_temp数组中，0到original_community_alones-1的行号记录的是原始网络中的社团检测情况。从行号为original_community_alones记录的是由于去除最大介数边的分裂情况。
+		int original_community_alones=0;
+
+		myGN GN_DEAL=new myGN(0);
+		GN_DEAL.GN_deal(vec_relationship,r_sign,vec_result_temp, vec_V_remove_belong);//GN算法的入口函数
+		original_community_alones=GN_DEAL.get_original_community_alones();
+		Q_i=GN_DEAL.get_Q_i();
+
+		result_temp_i=GN_DEAL.get_result_temp_i();
+		System.out.println("");
+
+		//如果原始网络时由几个孤立社团构成，要输出
+		//若原始网络本来是由几个孤立的社团组成的，要弄清楚，所求的社团数目是original_community_alones+1，因为original_community_alones的下标是从1开始的
+		int k3,out_temp=0;
+		if(original_community_alones!=0)
+		{
+			System.out.printf("\n原始网络是由%d个孤立的社团构成的：\n",original_community_alones+1);
+			for(j=0; j<original_community_alones+1; j++)
+			{
+				System.out.printf("\n集团%d：",(j+1));
+				for(k3=0; k3<max; k3++)
+				{
+					out_temp = vec_result_temp.elementAt(original_community_alones-1).point_belong_to[k3];
+					if(out_temp==j)
+					{
+						System.out.printf("%d  ",k3+1);
+					}									
+				}
+			}	
+		}
+		else
+		{
+			System.out.println("\n原始网络为：");
+			for(i=0; i<max; i++)
+			{
+				System.out.printf("%d  ",i+1);
+			}
+		}
+		//存放社区
+		HashMap<Integer,List<Integer>> Communitys=new HashMap<Integer,List<Integer>>();
+		
+		//最后把分裂的最佳情况输出，注意Q_i的下标是从0开始的。
+		//注意分裂后的社团数就是Q_i+2,存放在变量final_community_num中。
+		if(result_temp_i>original_community_alones){//通过result_temp_i>original_community_alones来判断原始网络是否经过GN算法分裂。
+			int final_community_num=Q_i+2;
+
+			System.out.println("\n\n\n\n经程序分析，原始网络极有可能分裂为"+final_community_num+"个集团：");
+
+			for(j=0; j<final_community_num; j++)
+			{
+				System.out.printf("\n集团%d：",(j+1));
+				//存放每个社区的list
+				ArrayList<Integer> clist = new ArrayList<>();
+			
+				for(i=0; i<max; i++)
+				{
+					out_temp = (int)vec_result_temp.elementAt(Q_i).point_belong_to[i];
+					if(out_temp==j)
+					{
+						clist.add(i+1);
+						System.out.printf("%d  ",i+1);
+					}
+
+				}
+				//存放每个社区
+				Communitys.put((j+1), clist);
+			}
+			
+			for(i=original_community_alones; i<=Q_i; i++)
+			{//从数目为original_community_alones+1的原始网络下开始分裂，到分裂为Q_i+2个社团结束。
+				//i是记录分裂情况的行号。i+2是分裂社团的个数
+				if(i!=original_community_alones)
+				{
+					System.out.printf("\n\n再");
+				}
+				else{
+					System.out.printf("\n\n在原始网络的基础上");
+				}
+				System.out.printf("去掉边(介数最大的边)：");
+				for(int k1 = 0; k1 < vec_V_remove_belong.size(); k1++)
+				{
+					if(vec_V_remove_belong.elementAt(k1).belong_clique == i+2)
+					{
+						System.out.printf("v(%d,%d),",vec_V_remove_belong.elementAt(k1).x,vec_V_remove_belong.elementAt(k1).y);
+					}
+				}
+			}	  
+		}	
+		System.out.printf("\n");
+		long end=System.currentTimeMillis();
+		//获取邻接矩阵
+		adjacencyListChangeadjacencyMatrix(adjacencyList,vertexNumber);
+		System.out.println("Q:"+ getQ( Communitys,Edge_graph,adjacencyMatrix));
+		System.out.println("算法运行时间为："+(end-start)+"ms");
+	}
+	
+}
---
+++ src/com/nieyue/lpa/LPA_file2.java
@@ -0,0 +1,281 @@
+package com.nieyue.lpa;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public class LPA_file2 {
+	static int Vertex = 115; //解决N个节点的LPA算法	
+	static int [][] Adjmartrix=new int[Vertex][Vertex];//图的邻接矩阵形式
+	static List<int[]> Edge_graph = null;//图中所有的边
+	static int[] Lable_t=new int[Vertex];//存放各个点的标签,t时刻，采用异步更新，更新过的lable与未更新的lable同时记录Lable_t中
+	static int[] Lable_t_1=new int[Vertex];//存放各个点的标签,t-1时刻，用于判断划分是否结束
+	static int[] Importance_sorting=new int[Vertex];//存放各个点的重要度，即
+	static int[] Vertex_neighbour_lable=new int[Vertex];//统计v的邻接节点中标签的个数，下标为标签，值为标签为该值的节点个数
+	static int MaxIteration = 50; //循环阈值
+	
+	static Map<Integer, List<Integer>> Communitys = null;//将标签作为键，节点序号列表作为值，保存到Map中
+
+	/**********************数据初始化 从文件中读取图的内容*************************/
+ 	public static void Init_Graph(String filename){//
+		File file = new File(filename);
+    	String ecoding = "gbk";
+		if(file.isFile() && file.exists() ){
+			InputStreamReader inputStreamReader = null;
+			BufferedReader bufferedReader = null;
+			Edge_graph = new ArrayList<int[]>();
+			try {
+				inputStreamReader = new InputStreamReader( new FileInputStream(file),ecoding);
+				bufferedReader = new BufferedReader(inputStreamReader);
+				String inputLine = null;
+//				inputLine = bufferedReader.readLine(); //先默认处理7个节点的情况
+//				String[] snodeNum=inputLine.split("-");
+//				int nodeNum=Integer.parseInt(snodeNum[0]);
+				//用二维数组表示原始图，需要注意的是，顶点序号减一才能使二维数组的下标与边的顶点的序号对应，
+				while( ( inputLine = bufferedReader.readLine() ) != null ){
+					String[] edge=inputLine.split("-");
+					//从输入数据构造以二维数组形式表示的图
+					for(int k = 0; k < edge.length; k++ ){
+						String[] edgePoint = edge[k].split(":");
+						int x1=Integer.parseInt(edgePoint[0]);
+						int x2=Integer.parseInt(edgePoint[1]);
+						Adjmartrix[x1-1][x2-1]=1;
+						Adjmartrix[x2-1][x1-1]=1;
+						int[] edgepoints = {x1,x2};
+						Edge_graph.add(edgepoints);
+					}
+				}
+				for(int i=0;i<Vertex;i++){//对每个节点进行赋值标签，初始化为下标值
+					Lable_t[i]=i;
+					Importance_sorting[i]=0;
+					Lable_t_1[i]=0;
+					Vertex_neighbour_lable[i]=0;
+				}				
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println(e.getMessage());
+			}finally {
+				try {
+					bufferedReader.close();
+					inputStreamReader.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+ 	
+	/**********************对重要度进行排序,重要度即为节点的度数*************************/
+	public static void Initing_Degree_Sorting(List<int[]> Edge){//对每个节点进行赋值标签，初始化为下标值
+		int[][] Degree_array_temp=new int[Vertex][2];
+		int max=-1;
+		int temp=0;
+		int[] visited=new int[Vertex];
+		for(int i=0;i<Vertex;i++){
+			Degree_array_temp[i][0]=0;
+			Degree_array_temp[i][1]=i;
+			visited[i]=0;
+		}
+		int x[] = null;
+		for(int i=0;i<Edge.size();i++){
+			x = Edge.get(i);
+			Degree_array_temp[x[0]-1][0]=Degree_array_temp[x[0]-1][0]+1;
+			Degree_array_temp[x[1]-1][0]=Degree_array_temp[x[1]-1][0]+1;
+		}
+		/*for(int i=0;i<Degree_array_temp.length;i++){
+			for(int j=0;j<Degree_array_temp.length;j++){
+				if(max<Degree_array_temp[j][0]&&visited[j]==0){
+					max=Degree_array_temp[j][0];
+					temp=j;
+				}	
+			}
+			max=-1;
+			visited[temp]=1;
+			Importance_sorting[i]=temp;
+		}*/
+		ArrayList<Integer> list=new ArrayList<>();
+		for(int i=0;i<Degree_array_temp.length;i++){
+				list.add(i);
+		}
+		 Collections.shuffle(list);
+		for(int i=0;i<list.size();i++){
+			Importance_sorting[i]=list.get(i);
+		}
+		
+	}
+	
+	
+	/**********************标签的选择更新策略*************************/
+	public static void Lable_Spread(){
+		for(int i=0;i<Vertex;i++)//记录节点的当前标签，用于判断循环是否结束
+			Lable_t_1[i]=Lable_t[i];	
+		Lable_Update();
+		//System.out.println("Lable_Spread:");
+	}
+	/**********************标签更新*************************/
+	public static void Lable_Update(){
+		for(int i=0;i<Vertex;i++){
+			if(Is_isolated_vertex(Importance_sorting[i])){
+				continue;//为节省时间，孤立节点直接跳过
+			}
+			else{//从节点temp的邻居节点中选一个节点，用该节点的标签作为temp的标签
+				int temp=Importance_sorting[i];
+				Lable_t[temp]=Lable_Select(temp);
+			}		
+		}
+		//System.out.println("Lable_Update:");
+	}
+	/**********************选择标签：从节点v的邻居节点中选一个节点，该节点的标签用来更新节点v的标签*************************/
+	public static int Lable_Select(int v){
+		int vertex_lable_select = 0;
+		Lable_count(v);	//统计v的邻接节点的标签及标签的个数，下标为标签，值为标签为该值的节点个数
+		int max=-1;
+		for(int i=0;i<Vertex;i++){
+			//从节点v的邻居节点中，选出标签个数最大的标签，用来更新节点v的标签，若有相同个数的标签，则选择第一个标签。
+			if(max<Vertex_neighbour_lable[Importance_sorting[i]]){
+				max=Vertex_neighbour_lable[Importance_sorting[i]];
+				vertex_lable_select=Importance_sorting[i];
+				}
+		}
+		for(int i=0;i<Vertex;i++){//为下一次的Lable_count()做准备
+			Vertex_neighbour_lable[i]=0;
+		}
+		//System.out.println("Lable_Select:");
+		return vertex_lable_select;
+	}
+	/**********************对邻居节点标签个数进行初始化,采用异步更新*************************/
+	//注意同步更新在处理二等分网络时，会引起震荡，无法收敛，转而采用异步更新
+	public static void Lable_count(int v){
+		//统计v的邻接节点中标签的个数，下标为标签，值为标签为该值的节点个数
+		for(int i=0;i<Adjmartrix.length;i++){
+			if(Adjmartrix[v][i]>0){//用当前时刻更新过的标签与未更新的标签对邻居节点标签个数进行初始化
+				Vertex_neighbour_lable[Lable_t[i]]=Vertex_neighbour_lable[Lable_t[i]]+1;
+			}
+		}
+		//System.out.println("Lable_count:");
+	}
+	/**********************判断是否是孤立节点*************************/
+	public static boolean Is_isolated_vertex(int v){
+		int temp=0;
+		for(int i=0;i<Adjmartrix.length;i++){
+			if(Adjmartrix[v][i]>0){//对邻居节点标签个数进行初始化
+				temp++;
+			}
+		}
+		//System.out.println("Is_isolated_vertex:");
+		if(temp>0)
+			return false;
+		else
+			return true;
+	}
+	
+	
+	/*总结划分结果,将社区标签作为键，社区内节点序号列表作为值，保存到Map中*/
+	public static void Community_divided() {
+		Communitys = new HashMap<Integer, List<Integer>>();
+		for(int i=0;i<Lable_t.length;i++){
+			if(Communitys.get(Lable_t[i])==null){
+				Communitys.put(Lable_t[i], new ArrayList<>());
+				Communitys.get(Lable_t[i]).add(i);
+			}else {
+				Communitys.get(Lable_t[i]).add(i);
+			}
+		}
+	}
+	/*计算模块度,参考博客：https://blog.csdn.net/wangyibo0201/article/details/52048248?locationNum=2 中的定义一*/
+	public static double getQ() {
+		double q = 0;
+		int communityNum = Communitys.size();
+		int[][] communityMtr = new int[communityNum][communityNum];
+		List<Integer> iList = null;
+		List<Integer> jList = null;
+		List<Integer> keyList = new ArrayList<>(Communitys.keySet());
+		for (int i_index=0; i_index<keyList.size();i_index++ ) {
+			iList = Communitys.get(keyList.get(i_index));
+			for (int j_index=0;j_index<keyList.size();j_index++) {
+				jList = Communitys.get(keyList.get(j_index));
+				for(int i=0;i<iList.size();i++){
+					for(int j=0;j<jList.size();j++){
+						communityMtr[i_index][j_index] += Adjmartrix[iList.get(i)][jList.get(j)];
+					}
+				}
+			}
+		}
+		float a=0,e=0;
+		double a_2 = 0;
+		for(int i=0;i<communityNum;i++){
+			e += communityMtr[i][i];
+			for(int j=0;j<communityNum;j++){
+				if(i==j)
+					continue;
+				a += communityMtr[i][j];
+			}
+			a_2 += ((0.5*a/(float)Edge_graph.size())*(0.5*a/(float)Edge_graph.size()));
+		}
+	/*	System.out.println("e=" + e + " a=" + a);
+		System.out.println("e=" + e + " a=" + a + " a_2=" + a_2);*/
+		q = e/Edge_graph.size()*0.5 - a_2;
+		return q;
+	}
+	
+	public static void main(String[] args) {
+		Long start=System.currentTimeMillis();
+		int Iteration=0;
+		/**********************初始化*************************/
+		String filename = "src/com/nieyue/lpa/Karate.txt";Vertex=34;
+		//String filename = "src/com/nieyue/lpa/Dolphins.txt";Vertex=62;
+		//String filename = "src/com/nieyue/lpa/PolBooks.txt";Vertex=105;
+		//String filename = "src/com/nieyue/lpa/Football.txt";Vertex=115;
+		Init_Graph(filename);
+		Initing_Degree_Sorting(Edge_graph);//对重要度进行排序，亦是随机序列
+		
+		
+		/*****************对每一个节点，返回所有邻居中某标签数最多的标签值*****************/
+		while(!Arrays.equals(Lable_t_1,Lable_t) && Iteration<MaxIteration){
+			//由于同步更新在处理二等分网络时的循环震荡问题，这里控制循环次数
+			Iteration++;
+			Lable_Spread();
+		}
+		
+		Community_divided();
+		
+		System.out.print("标签前的网络：");
+		for(int i=0;i<Vertex;i++){
+			System.out.print(i+" ");
+		}
+		System.out.print("\n");
+		
+		System.out.print("标签后的网络：");
+		for(int i=0;i<Vertex;i++){
+			System.out.print(Lable_t[i]+" ");
+		}
+		System.out.print("\n");
+		int key = 0;
+		List<Integer> value = null;
+		for (Iterator<Integer> iterator = Communitys.keySet().iterator(); iterator.hasNext();) {
+			key = (int) iterator.next();
+			System.out.print("标签为" + key + "的网络中的节点序号为：");
+			value = Communitys.get(key);
+			for(int i=0;i<value.size();i++){
+				System.out.print(value.get(i)+"\t");
+			}
+			System.out.print("\n");
+		}
+		System.out.println("划分后的模块度Q为：" + getQ());;
+		System.out.println("Iteration:"+Iteration);
+		Long end=System.currentTimeMillis();
+		System.out.println("算法运行时间:"+(end-start)+"ms");
+		
+	}
+}
+
